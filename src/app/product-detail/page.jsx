1d906733fb31e7f1b78485f1bb53558e333ce2a6
alex <you@example.com>
2024-06-20 23:16:32 +0530

    converted product-detail to dynamic

--- src/app/product-detail/page.jsx
+++ src/app/product-detail/page.jsx
@@ -1,8 +1,14 @@
 import { Suspense } from "react";
 import { Product } from "../components/product";
 import { Reviews } from "../components/reviews";
+import { cookies } from "next/headers";
 
 export default function ProductDetail() {
+
+   const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log("theme: ", theme);
+
   return (
     <div>
       <h1>Product Detail Page</h1>
